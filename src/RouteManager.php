0313f6716208c69f1ce7f87eea0621226c1de3f7
Sumon Ahmed <bdsumon4u@gmail.com>
2021-07-10 22:47:31 +0600

    Get Routable Methods

--- src/RouteManager.php
+++ src/RouteManager.php
@@ -4,6 +4,8 @@ namespace Hotash\AutoRoute;
 
 use Illuminate\Container\Container;
 use Illuminate\Support\Str;
+use ReflectionClass;
+use ReflectionMethod;
 
 class RouteManager
 {
@@ -32,6 +34,7 @@ class RouteManager
      *
      * @param array|string $controller
      * @param array $options
+     * @throws \ReflectionException
      */
     public function generate($controller, array $options = []): void
     {
@@ -60,9 +63,54 @@ class RouteManager
      * @param string $prefix
      * @param string $controller
      * @param array $options
+     * @throws \ReflectionException
      */
     public function route(string $prefix, string $controller, array $options = []): void
     {
-        //
+        $as = str_replace('/', '.', trim($prefix, '/')) . '.';
+        $options = array_merge(compact('prefix', 'as'), $options);
+
+        foreach ($this->getRoutableMethods($controller, $options) as $method) {
+            dump($method);
+        }
+    }
+
+    /**
+     * Get routable methods of a controller.
+     *
+     * @param string $controller
+     * @param array $options
+     * @return array
+     * @throws \ReflectionException
+     */
+    private function getRoutableMethods(string $controller, array $options): array
+    {
+        $class = new ReflectionClass($controller);
+
+        $only = $options['only'] ?? [];
+        $except = $options['except'] ?? [];
+
+        return collect($class->getMethods(ReflectionMethod::IS_PUBLIC))
+            ->map(function ($method) use ($controller, $only, $except) {
+                if ($method->class !== $controller) {
+                    return null;
+                }
+
+                if (!empty($only) && !in_array($method->name, $only, true)) {
+                    return null;
+                }
+
+                if (!empty($except) && in_array($method->name, $except, true)) {
+                    return null;
+                }
+
+                if ($method->name !== '__invoke' && Str::startsWith($method->name, '__')) {
+                    return null;
+                }
+
+                return $method;
+            })
+            ->filter()
+            ->toArray();
     }
 }
